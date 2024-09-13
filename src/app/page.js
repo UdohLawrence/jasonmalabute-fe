c493b922c7483117cf4e1af7446c5fde179f1d2c
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-09-13 14:54:37 +0100

    UPDATE: Call-to-Action on the Hero Section

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
                         href="#"
                         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
                     >
-                        Get started
+                        Book a Call
                     </Link>
                     <Link
                         href="#"
