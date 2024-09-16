875c6c5935e19ba49f3257e0fed6fcfbbdb47a78
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-09-16 12:09:44 +0100

    FIX: prevent accordion from expanding adjacent cards

--- src/components/Services.js
+++ src/components/Services.js
@@ -25,42 +25,42 @@ const Services = () => {
       description:
         "Good accounting and bookkeeping are crucial for real estate investors and passive investors to accurately track the financial performance of their properties. Our specialized accounting services provide detailed financial reports and cash flow monitoring, helping you make informed decisions while saving valuable time.",
       imageUrl: "/accounting.jpg",
-      alt: "",
+      alt: "Accounting",
     },
     {
       title: "Tax Planning",
       description:
         "We stay in regular contact with you throughout the year to plan and maximize your tax savings. Our focus is on high-net-worth individuals and high-income earners involved in real estate, ensuring you get the best strategies for your financial success.",
       imageUrl: "/planning.jpg",
-      alt: "",
+      alt: "Tax Planning",
     },
     {
       title: "Tax Preparation",
       description:
         "Ensure your tax documents are prepared and distributed on time, whether it’s for your individual tax needs or meeting the demands of your passive investors. Our timely and accurate tax preparation services are designed to keep you compliant and stress-free.",
       imageUrl: "/preparation.jpg",
-      alt: "",
+      alt: "Tax Preparation",
     },
     {
       title: "Payroll Services",
       description:
         "Manage payroll efficiently and stay compliant with state and federal payroll tax laws.",
       imageUrl: "/payroll.jpg",
-      alt: "",
+      alt: "Payroll Services",
     },
     {
       title: "Advisory",
       description:
         "Get expert advice on structuring and growing your real estate investments for success. We help you make informed decisions and optimize your real estate investment portfolio.",
       imageUrl: "/advisory.jpg",
-      alt: "",
+      alt: "Advisory",
     },
     {
       title: "Tax Smart Investors",
       description:
         "Receive professional tax advice from our real estate experts at a fraction of the cost of one on one advisory services.",
       imageUrl: "/investors.jpg",
-      alt: "",
+      alt: "Tax Smart Investors",
     },
   ];
   return (
@@ -72,28 +72,34 @@ const Services = () => {
       >
         <div className="inset-0 backdrop-blur-md rounded-lg bg-black/30 max-w-6xl w-full mx-auto my-3 py-3 px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
           {servicesList.map((service, index) => (
-            <Card key={index} className="mt-6 max-w-sm mx-auto p-3 rounded-md">
-              <CardHeader
-                color="blue-gray"
-                className="relative h-56 rounded-lg"
+            <Accordion open={expandedCard === index}>
+              <Card
+                key={index}
+                className="mt-6 max-w-sm mx-auto p-3 rounded-md"
               >
-                <Image src={service.imageUrl} fill alt={service.alt} />
-              </CardHeader>
-              <CardBody>
-                <Accordion open={expandedCard === index}>
+                <CardHeader
+                  color="blue-gray"
+                  className="relative h-56 rounded-lg"
+                >
+                  {expandedCard === index ? (
+                    <AccordionBody onClick={() => toggleAccordion(index)}>
+                      <Typography className="text-sm text-justify px-3">
+                        {service.description}
+                      </Typography>
+                    </AccordionBody>
+                  ) : (
+                    <Image src={service.imageUrl} fill alt={service.alt} />
+                  )}
+                </CardHeader>
+                <CardBody>
                   <AccordionHeader onClick={() => toggleAccordion(index)}>
                     <Typography variant="h5" color="blue-gray">
                       {service.title}
                     </Typography>
                   </AccordionHeader>
-                  <AccordionBody onClick={() => toggleAccordion(index)}>
-                    <Typography className="text-sm text-justify">
-                      {service.description}
-                    </Typography>
-                  </AccordionBody>
-                </Accordion>
-              </CardBody>
-            </Card>
+                </CardBody>
+              </Card>
+            </Accordion>
           ))}
         </div>
       </section>
